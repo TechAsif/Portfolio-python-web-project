7d7dd5ae90d6cacf243b2b28573ebdedb97166b1
Äsif parvez <asifparvezsarker26@gmail.com>
2018-09-13 22:40:37 +0600

    App add in admin

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class  Job(models.Model):
+	"""docstring for  Job"""
+	image=models.ImageField(upload_to='images/')
+	summery=models.CharField(max_length=200)
+
