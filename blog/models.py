d83fb14882a172b61b5e97c0032eafc265f3fa91
Äsif parvez <asifparvezsarker26@gmail.com>
2018-09-14 18:29:38 +0600

    job apps is finished succesfully

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class  Blog(models.Model):
+	"""This is our blog class"""
+	title=models.CharField(max_length=200)
+	pub_date=models.DateTimeField()
+	body = models.TextField()
+	image=models.ImageField(upload_to='images/')
+	
